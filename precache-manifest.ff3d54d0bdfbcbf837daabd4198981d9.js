3f78ef3cbb314d9091a40c0120778c378a9e3727
Robertas Liekis <robertasliekis@gmail.com>
2021-04-05 20:45:17 +0300

    Updates

---
+++ precache-manifest.ff3d54d0bdfbcbf837daabd4198981d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a942dee88bd8712f8a36ad4c50e726",
+    "url": "//robertasliekis.github.io/power-plant-3d/index.html"
+  },
+  {
+    "revision": "86f88c6b3a6db187edb1",
+    "url": "//robertasliekis.github.io/power-plant-3d/static/css/main.d27678ba.chunk.css"
+  },
+  {
+    "revision": "635384c89096714d5ee7",
+    "url": "//robertasliekis.github.io/power-plant-3d/static/js/2.cd152954.chunk.js"
+  },
+  {
+    "revision": "f300e66b4bd3259a5ac97365de51abe3",
+    "url": "//robertasliekis.github.io/power-plant-3d/static/js/2.cd152954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86f88c6b3a6db187edb1",
+    "url": "//robertasliekis.github.io/power-plant-3d/static/js/main.989d4b93.chunk.js"
+  },
+  {
+    "revision": "96c0f5dc04f899662d39",
+    "url": "//robertasliekis.github.io/power-plant-3d/static/js/runtime-main.978fef1a.js"
+  }
+]);
